be93539aacd7c263ab71aac7b99e7c833290be39
BWKline81 <bwkline@icloud.com>
2025-08-24 11:32:20 -0400

    Further publising additions.

--- client/client/vite.config.js
+++ client/client/vite.config.js
@@ -4,10 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   build: {
     rollupOptions: {
       external: ["sweetalert2"],
+    },
+    mimeTypes: {
+      "*.js": "application/javascript",
     },
   },
 });
